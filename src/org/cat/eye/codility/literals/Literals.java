9415d0c06dbbcce5e0aa939200e25ae2e5108494
akotov <cat-kotov@yandex.ru>
2022-05-17 15:51:49 +0300

    Lesson 15

---
+++ src/org/cat/eye/codility/literals/Literals.java
@@ -0,0 +1,16 @@
+package org.cat.eye.codility.literals;
+
+public class Literals {
+
+    public static void main(String[] argv) {
+
+        Integer b1 = 128;
+        Integer b2 = 128;
+
+        Integer b11 = -127;
+        Integer b22 = -127;
+
+        System.out.println(b1 == b2);
+        System.out.println(b11 == b22);
+    }
+}
