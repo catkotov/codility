815c7c3a77cafe786053fda4f059e939c35045d1
Александр Котов <cat.kotov@gmail.com>
2024-04-25 18:40:44 +0300

    Добавлен алгоритм поиска элемента в массивах

---
+++ src/org/cat/eye/algorithms/interview/ElementSearchInArrays.java
@@ -0,0 +1,16 @@
+package org.cat.eye.algorithms.interview;
+
+/**
+ * Поиск элемента, который присутствует во всех трех массивах.
+ * Пример: даны три массива {1,2,4,5}, {3,3,4} и {2,3,4,5,6},
+ * результатом должен быть элемент 4.
+ */
+public class ElementSearchInArrays {
+
+    public static int searchSameElement(int[] a, int[] b, int[] c) {
+
+
+        return 0;
+    }
+
+}
